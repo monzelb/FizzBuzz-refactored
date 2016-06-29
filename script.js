1ce36dd02bc5ee5c561ea8f82a8fcd704f5ff600
monzelb <monzelb@gmail.com>
2016-06-29 13:27:51 -0700

    commit message

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
+
+
 $(document).ready(function() {
+
+
 function fizzBuzz(){
+    var input = prompt("Please enter a number");
+    var integer = parseInt(input);
   var i=0;
-  for (i = 1; i <= 100; i ++) {
+  for (i = 1; i <= integer; i ++) {
     if (i % 3===0) { 
-    	if (i%5===0){
-    		document.write("FizzBuzz <br />")
-    	}
-    	else {
-    		document.write("Fizz <br />")
-    	}
+        if (i%5===0){
+            document.write("FizzBuzz <br />")
+        }
+        else {
+            document.write("Fizz <br />")
+        }
     }
     else if (i%5===0){
-    	document.write("Buzz <br />")
+        document.write("Buzz <br />")
     }
     else{
-    	document.write(i + "<br />")
+        document.write(i + "<br />")
     }
   }
 };
